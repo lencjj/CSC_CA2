3a9d3f781f6d71e0777cbc6d6ab7885870372338
enkr1 <enkr99@gmail.com>
2020-08-03 03:18:29 +0800

    SQLDatabase - all methods

---
+++ CA2_Talents_Webapp/Models/Talent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CA2_Talents_Webapp.Models
+{
+    public class Talent
+    {
+        public int TalentId { get; set; }
+        public string TalentName { get; set; }
+        public string TalentTitle { get; set; }
+        public string TalentDesc { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
